b4e8a89abc899abb2e881fa509726cfa165ee97d
Fernando Bártolo <git@fbart.net>
2022-12-21 20:23:55 +0000

    bugfix

--- models/all_dates.sql
+++ models/all_dates.sql
@@ -2,7 +2,7 @@
 
 {{ dbt_utils.date_spine(
     datepart="day",
-    start_date="to_date('01/01/2020', 'mm/dd/yyyy')",
-    end_date="to_date('01/01/2021', 'mm/dd/yyyy')"
+    start_date="to_date('01/01/2020', 'mm/dd/yyyy')", 
+    end_date="to_date('01/01/2021', 'mm/dd/yyyy')" 
 )
 }}
