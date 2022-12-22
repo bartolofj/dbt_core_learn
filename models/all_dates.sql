37a1cdd13d8d8403d32f27071676a61e90b3c157
Fernando Bartolo <git@fbart.net>
2022-12-22 11:55:10 +0000

    switch to duckdb

--- models/all_dates.sql
+++ models/all_dates.sql
@@ -2,7 +2,7 @@
 
 {{ dbt_utils.date_spine(
     datepart="day",
-    start_date="to_date('01/01/2020', 'mm/dd/yyyy')", 
-    end_date="to_date('01/01/2021', 'mm/dd/yyyy')" 
+    start_date="make_date(2020,01,01)", 
+    end_date="make_date(2021,01,01)" 
 )
 }}
--- models/staging/jaffle_shop/stg_customers.sql
+++ models/staging/jaffle_shop/stg_customers.sql
@@ -1,11 +1,10 @@
-with customers as (
+with
+    customers as (
 
-    select
-        id as customer_id,
-        first_name,
-        last_name
+        select id as customer_id, first_name, last_name
+        from {{ source("jaffle_shop", "customers") }}
 
-    from {{ source('jaffle_shop', 'customers')}}
+    )
+select *
 
-)
-select * from customers
+from customers
